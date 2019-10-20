50242478b042447dfcec1be47e1fe081f76ca1bd
RadheTians <radheramantiwari87@gmail.com>
2019-10-20 11:49:51 +0530

    second Commit

--- app/src/main/java/com/example/iiitmstars/MainActivity.java
+++ app/src/main/java/com/example/iiitmstars/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.iiitmstars;
 import android.os.Bundle;
 
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.iiitmstars.ui.aboutiiitm.AboutIIITMFragment;
+import com.example.iiitmstars.ui.academics.AcademicsFragment;
+import com.example.iiitmstars.ui.contactus.ContactUsFragment;
+import com.example.iiitmstars.ui.department.DepartmentFragment;
+import com.example.iiitmstars.ui.info.InfoFragment;
+import com.example.iiitmstars.ui.people.PeopleFragment;
+import com.example.iiitmstars.ui.tnp.TnpFragment;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -15,11 +26,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.iiitmstars.R.id.drawer_layout;
 
-    private AppBarConfiguration mAppBarConfiguration;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,27 +42,95 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_aboutiiitm, R.id.nav_academics, R.id.nav_department, R.id.nav_people, R.id.nav_info, R.id.nav_contactus)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displaySelectedScreen(int itemId) {
+
+        //creating fragment object
+        Fragment fragment = null;
+
+        //initializing the fragment object which is selected
+        switch (itemId) {
+            case R.id.nav_aboutiiitm:
+                fragment = new AboutIIITMFragment();
+                break;
+            case R.id.nav_academics:
+                fragment = new AcademicsFragment();
+                break;
+            case R.id.nav_contactus:
+                fragment = new ContactUsFragment();
+                break;
+            case R.id.nav_department:
+                fragment = new DepartmentFragment();
+                break;
+            case R.id.nav_info:
+                fragment = new InfoFragment();
+                break;
+            case R.id.nav_people:
+                fragment = new PeopleFragment();
+                break;
+
+            case R.id.nav_tnp:
+                fragment = new TnpFragment();
+                break;
+        }
+
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.nav_host_fragment, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        //calling the method displayselectedscreen and passing the id of selected menu
+        displaySelectedScreen(item.getItemId());
+        //make this method blank
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
--- app/src/main/java/com/example/iiitmstars/ui/aboutiiitm/AboutUsActivity.java
+++ app/src/main/java/com/example/iiitmstars/ui/aboutiiitm/AboutUsActivity.java
@@ -1,9 +1,14 @@
 package com.example.iiitmstars.ui.aboutiiitm;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 
 import com.example.iiitmstars.R;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class AboutUsActivity extends AppCompatActivity {
@@ -12,6 +17,7 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
 
     }
 
